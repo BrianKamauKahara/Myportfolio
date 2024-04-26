5f987d8ac8dee2960cbb4e770252d1f801f67976
emeraldzs <theemeraldprogamer@gmail.com>
2024-04-26 16:53:17 +0300

    working socials button

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,8 @@
 const initApp = () => {
     const hamburgerBtn = document.getElementById('hb-button')
     const dropdownMenu = document.getElementById('dropdown')
+    const socialBtn = document.getElementById('social-button')
+    const socialMenu = document.getElementById('social-menu')
 
     const toggleMenu = () => {
         hamburgerBtn.classList.toggle('button-anim')
@@ -8,8 +10,13 @@ const initApp = () => {
         dropdownMenu.classList.toggle('flex')
 
     }
+    const toggleSocial = () => {
+        socialMenu.classList.toggle('flex')
+        socialMenu.classList.toggle('hidden')
+    }
 
     hamburgerBtn.addEventListener('click', toggleMenu)
+    socialBtn.addEventListener('click', toggleSocial)
 }
 
 document.addEventListener('DOMContentLoaded', initApp)
